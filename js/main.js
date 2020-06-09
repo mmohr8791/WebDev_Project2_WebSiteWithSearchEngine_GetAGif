bd55fa43178719ffc111814e6f6103ada0f00cd5
mike.mohr <mmohr.55987@gmail.com>
2020-06-09 09:53:04 -0500

    Adding code changes to main.js file

--- js/main.js
+++ js/main.js
@@ -1,17 +1,43 @@
 // main.js //
 
-$(document).ready(()=>{
-    $("submit").click(()=>{
-        let userInput = $("#search").val()
-        // alert(userInput)
-        $.ajax({
-            url:""
-        })
-        .done((res)=>{
-            console.log("res", res.Search)
-        })
-    })
+const searchForm = document.querySelector('.js-search-form');
+const searchInput = document.querySelector('.js-search-input');
+const resultsEl = document.querySelector('.js-results');
+const searchNum = document.querySelector('.js-search-num');
+
+searchForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const q = searchInput.value;
+    const limit = searchNum.value;
+    search(q, limit);
 })
 
+function search (q, limit) {
+    const apiKey = "?";
+    const path = '?'
+}
 
+fetch(path)
+    .then(function(res) {
+        return res.json()
+    })
+
+    .then(function(json) {
+        let resultsHTML = "json.data.forEach(function(obj) {
+            const url = obj.images.fixed_width.url
+            const width = obj.images.fixed_width.height
+            const height = obj.images.fixed_width.height
+            const alt = obj.title
+            resultsHTML +=
+                <div class="box">
+                <div class="content">
+                    <figure>
+                        <img src="${url}" width="${width}" height="${height}" alt="${alt}"></img>
+                        <figcaption>${alt}</figcaption>
+                    </figure>
+                </div>
+            </div>
+        })
+        resultsEl.innerHTML = resultsHTML;
+    })
 
