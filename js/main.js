b81728ee0bbfb2aae6be8294a9fe0c1d090ff58e
mike.mohr <mmohr.55987@gmail.com>
2020-06-10 18:57:43 -0500

    Adding input validation in main.js file

--- js/main.js
+++ js/main.js
@@ -5,6 +5,16 @@ const searchInput = document.querySelector('.js-search-input');
 const resultsEl = document.querySelector('.js-results');
 const searchNum = document.querySelector('.js-search-num');
 
+function lettersAndNumbersOnly(input) {
+    var regex = /[^a-z,0-9,-, ]/gi;
+    input.value = input.value.replace(regex, "");
+}
+
+function numbersOnly(input) {
+    var regex = /[^0-9]/g;
+    input.value = input.value.replace(regex, "");
+}
+
 searchForm.addEventListener('submit', function(e) {
     e.preventDefault();
     const q = searchInput.value;
@@ -13,8 +23,8 @@ searchForm.addEventListener('submit', function(e) {
 })
 
 function search (q, limit) {
-    const apiKey = "?";
-    const path = '?'
+    const path = 'api.giphy.com/v1/gifs/search';
+    const apiKey = "4pXNXgB40a4SilkpPfI2ZlLu1qR6utmm";
 }
 
 fetch(path)
@@ -23,20 +33,22 @@ fetch(path)
     })
 
     .then(function(json) {
-        let resultsHTML = "json.data.forEach(function(obj) {
+        let resultsHTML = ''
+            json.data.forEach(function(obj) {
             const url = obj.images.fixed_width.url
-            const width = obj.images.fixed_width.height
+            const width = obj.images.fixed_width.width
             const height = obj.images.fixed_width.height
-            const alt = obj.title
-            resultsHTML +=
-                <div class="box">
+            const alt = obj.title;
+            resultsHTML += `
+            <div class="box">
                 <div class="content">
                     <figure>
-                        <img src="${url}" width="${width}" height="${height}" alt="${alt}"></img>
+                        <img src="${url}" width="${width}" height="${height}" alt="${alt}">
                         <figcaption>${alt}</figcaption>
                     </figure>
                 </div>
             </div>
+            `
         })
         resultsEl.innerHTML = resultsHTML;
     })
